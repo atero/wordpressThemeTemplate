4234322c4089c593107e5a6cf559da4486df8df8
Karen Chobanyan <karenchobanyan@Karens-Mac-Pro-2.local>
2014-04-27 20:54:17 +0400

    initial

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> <?php the_title(); ?> </title>
+	<meta charset="utf-8">
+	<?php wp_head(); ?>
+	<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/style.css">
+	<script type="text/javascript" src="<?php echo bloginfo('template_url'); ?>/js/script.js"></script>
+</head>
+<body>
+
+</body>
+</html>
